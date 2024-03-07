37c864480b489dab77e970efa80852f1e8421ff6
Akashh001 <protongithub1@protonmail.com>
2024-03-08 00:13:19 +0530

    add comment section

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -4,6 +4,7 @@ import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
 import DashPosts from '../components/DashPosts';
 import DashUsers from '../components/DashUsers';
+import DashUsers from '../components/DashUsers';
 
 export default function Dashboard() {
   const location = useLocation();
